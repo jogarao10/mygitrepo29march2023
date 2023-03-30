9f6d7074ddc8e2ab25953627374b9acde2db0e5d
vijay <vijaykorrayi107@gmail.com>
2023-03-30 14:22:58 +0530

    my emp3 file

--- myEmp2file.java
+++ myEmp2file.java
@@ -1,8 +1,16 @@
 This is java programing
+<<<<<<< Updated upstream
 1st lines of code
 2nd lines of code
 3rd lines of code
 4th lines of code
 
 this is my urgent work by assgined by manager
+=======
+1st day lines of code
+2nd day lines of code
+3rd day lines of code
+4th day lines of code
+5th day lines of code
+>>>>>>> Stashed changes
 
